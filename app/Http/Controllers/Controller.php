af3447c360e314d41922f4c1a8a1cf24f1900054
rn <associate.ru@outlook.com>
2017-04-04 11:39:05 +0700

    Modified Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,23 +1,11 @@
 <?php
 
-// namespace App\Http\Controllers;
-
-// use Illuminate\Foundation\Bus\DispatchesJobs;
-// use Illuminate\Routing\Controller as BaseController;
-// use Illuminate\Foundation\Validation\ValidatesRequests;
-// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
-// class Controller extends BaseController
-// {
-//     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-// }
-
 namespace App\Http\Controllers;
 
 use Route;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
